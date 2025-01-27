ca46acc32440926d864cf96b06da9d212ed72c0a
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2025-01-27 14:55:24 -0300

    Squashed commit of the following:
    commit 0115e07258d4c4611c67b7e1b9f6b7bfd6908d8a
    Merge: 478d59f 2c84ba2
    Author: Fernando Hali <100550886+fernandohali@users.noreply.github.com>
    Date:   Mon Jan 27 10:52:30 2025 -0300
    
        Merge pull request #5 from Embarcatech-SubGrupo9/teclado_matricial
    
        Teclado matricial
    
    commit 2c84ba226db94c761841da8461b167433c4c926e
    Author: Fernando Hali <100550886+fernandohali@users.noreply.github.com>
    Date:   Mon Jan 27 10:51:09 2025 -0300
    
        Teclado funcionando no wokwi
    
    commit 7da52fdf9c3e7d986c92cd12055f16d407a68389
    Author: Fernando Hali <100550886+fernandohali@users.noreply.github.com>
    Date:   Mon Jan 27 09:48:41 2025 -0300
    
        Colocando a função do teclado em pasta.
    
    commit 478d59f41073acdd4280c5a3b169b3874aa59dc4
    Author: Fernando Hali <100550886+fernandohali@users.noreply.github.com>
    Date:   Sun Jan 26 19:53:12 2025 -0300
    
        Configuração os cabos do simulador wokwi
    
    commit 484f3b1e5b07a284151ecda3a67141f77cd2fd83
    Author: Fernando Hali <100550886+fernandohali@users.noreply.github.com>
    Date:   Sun Jan 26 19:42:00 2025 -0300
    
        Revert "Merge pull request #4 from Embarcatech-SubGrupo9/Leds-White"
    
        This reverts commit 3a0ce8e983f6b22b9f7cb919a4d27a1d57d31099, reversing
        changes made to 38831fef051ed4508437a06606e5022905a017aa.
    
    commit 3a0ce8e983f6b22b9f7cb919a4d27a1d57d31099
    Merge: 38831fe 45f1318
    Author: Fernando Hali <100550886+fernandohali@users.noreply.github.com>
    Date:   Sun Jan 26 19:40:24 2025 -0300
    
        Merge pull request #4 from Embarcatech-SubGrupo9/Leds-White
    
        Correção dos cabos para a matriz de leds.

---
+++ teclado/tecladoMatricial.h
@@ -0,0 +1,11 @@
+#ifndef _TECLADOMATRICIAL_H
+#define _TECLADOMATRICIAL_H
+
+// Função para configurar os pinos do teclado
+void inicializar_pinos();
+
+
+// Função para ler uma tecla pressionada
+char verificar_tecla();
+
+#endif
